5542249a8cdfbe603189a192df584f034fb32d49
xueying207 <chenmiao@8760S.com>
2016-12-27 22:11:21 +0800

    1
    1

--- wangpiao/miaotest/test/apitest/data.py
+++ wangpiao/miaotest/test/apitest/data.py
@@ -5,14 +5,14 @@ http="https://testbj.wangpiao.com/WebConfidential/WebMonitorService.asmx/"
 key= "test"
 deskey="eltcwang"
 UserName="Monitor"
-Password="9AXtDfLmLH2YnuT0"
+Password="123456"
 Des_IV = "\x12\x34\x56\x78\x90\xAB\xCD\xEF" # 自定IV向量
 
 def test2():
     dir1='E:/apitest/hytest/'
     http="http://wifi.api.wangpiao.com/WebConfidential/WPConfidentialService.asmx/"
-    key= "MOWangpiao.comYx@C7KR2H=)"
+    key= "test"
     deskey="eltcwang"
     UserName="WPHeiying"
-    Password="Wah0@^OPCPy32QLv"
+    Password="123456"
     Des_IV = "\x12\x34\x56\x78\x90\xAB\xCD\xEF" # 自定IV向量
